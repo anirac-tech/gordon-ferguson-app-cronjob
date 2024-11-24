f351b9ea129ed71090b9279d2c9a45580778f5df
alexdivadi <dawork20100@gmail.com>
2024-11-23 17:20:15 -0800

    Update log function

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'dart:async';
-import 'dart:developer';
-
 import 'package:starter_template/job.dart';
 
 // This Appwrite function will be executed every time your function is triggered
 Future<dynamic> main(final context) async {
   try {
     final result = await run();
+    context.log(result);
     return context.res.json({"code": 200, "message": result});
   } catch (e) {
-    log('$e', error: e);
+    context.log('$e', error: e);
     return context.res.json({
       "error": {"code": 500, "message": "$e"}
     });
